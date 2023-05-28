f11e1e8a0449035c1f681e896bcd82b48bde502f
arvin <arvind.bcamca@gmail.com>
2023-05-27 23:44:49 -0400

    add new changes

--- src/main/scala/SampleApp.scala
+++ src/main/scala/SampleApp.scala
@@ -3,6 +3,7 @@ import org.apache.spark.sql.SparkSession
 object SampleApp {
 
   def main(args:Array[String]): Unit ={
+    import spark.implicits._
 
     val spark = SparkSession.builder()
       .master("spark://spark:7077")
@@ -15,7 +16,13 @@ object SampleApp {
     println("Master :"+spark.sparkContext.master);
     val rdd=spark.read.textFile("hello.txt")
     println("word count of hello.txt")
-    rdd.flatMap(x=>x.split(" ")).map(x=>(x,1)).reduceByKey(_+_).collect()
+
+    val rdd1 =rdd.flatMap(x=>x.split(" ")).map(x=>(x,1)).collect()
+    rdd1.foreach(x=> {
+      println(x._1)
+      println(x._2)
+    }
+    )
 
   }
 }
